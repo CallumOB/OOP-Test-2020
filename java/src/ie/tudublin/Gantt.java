b3a372ccb3694a14f81fbba4072371f4a3a7b834
CallumOB <callumobrien2004@gmail.com>
2023-04-26 11:19:53 +0100

    Finished displayTasks() and removed ganttSetup()

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -8,7 +8,9 @@ import processing.data.TableRow;
 
 public class Gantt extends PApplet
 {	
-
+	private int numLines = 30;
+	private int namesPart = 150; // the width of the section containing the names
+	private float rowHeight = 40;
 	ArrayList<Task> tasks = new ArrayList<>();
 	
 	public void settings()
@@ -46,6 +48,7 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		colorMode(HSB);
 		loadTasks();
 		printTasks();
 	}
@@ -53,25 +56,36 @@ public class Gantt extends PApplet
 	public void draw()
 	{			
 		background(0);
-		ganttSetup();
+		displayTasks();
 	}
 
 	public void displayTasks() {
-		float colour = 255 / (float) tasks.size();
-		
-		for (Task task : tasks) {
-			
-		}
-	}
-
-	public void ganttSetup() {
 		int offset = 30;
-		int gap = 0;
-		int numLines = 30;
+
+		textSize(12);
+		textAlign(CENTER, CENTER);
+
 		stroke(255);
-		for (int i = 0; i < numLines; i++) {
-			line(offset + gap, offset, offset + gap, height - offset);
-			gap += 20;
+
+		for (int i = 1; i <= numLines; i++) {
+			float x = map(i, 1, numLines, namesPart, width - offset);
+			line(x, offset, x, height - offset);
+			fill(255);
+			text(i, x, offset * 0.5f);
+		}
+
+		textAlign(LEFT, CENTER);
+
+		for (int i = 0; i < tasks.size(); i++) {
+			float y = (offset * 2) + rowHeight * i;
+			fill(255);
+			text(tasks.get(i).getTaskDesc(), offset, y);
+
+			noStroke();
+			fill(map(i, 0, tasks.size(), 0, 255), 255, 255);
+			float x1 = map(tasks.get(i).getStart(), 1, numLines, namesPart, width - offset);
+			float x2 = map(tasks.get(i).getEnd(), 1, numLines, namesPart, width - offset);
+			rect(x1, y - 15, x2 - x1, rowHeight - 5, 5.0f);
 		}
 	}
 }
